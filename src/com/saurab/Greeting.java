b5870a053689b30ad2ff6f0cdd2abd079a42b837
SaurabPoudel <saurabpoudel60@gmail.com>
2021-08-19 14:28:17 +0545

    Greeting method is added

---
+++ src/com/saurab/Greeting.java
@@ -0,0 +1,13 @@
+package com.saurab;
+
+public class Greeting {
+    public static void main(String[] args) {
+        int i;
+        for (i=0;i<10;i++){
+            greeting();
+        }
+    }
+    static void greeting(){
+        System.out.println("Hello world");
+    }
+}
