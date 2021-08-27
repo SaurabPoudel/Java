1358e8957e9710af5ba2d47732265d703e8d0a58
SaurabPoudel <saurabpoudel60@gmail.com>
2021-08-27 15:30:30 +0545

    Floor and Ceiling binary search added

---
+++ Java Search/src/com/saurab/CeilingNumber.java
@@ -0,0 +1,31 @@
+package com.saurab;
+
+public class CeilingNumber {
+    public static void main(String[] args) {
+        int[] arr = {-9,-6,-2,3,4,7,13,23,30,32,35};//shorted array
+        int target =20;
+        int ans = ceiling(arr,target);
+        System.out.println(ans);
+    }
+    // return the index
+    // return -1 if element doesn't exist
+    static int ceiling(int[] arr,int target){
+        int start = 0;
+        int end = arr.length -1;
+        while(start <= end){
+            // find the middle element
+            // int mid = (start + end) can exceed the range of int, so we can do following step
+            int mid = start + (end - start)/2;
+            if(target < arr[mid] ){
+                end = mid -1;
+            }
+            if(target > arr[mid]){
+                start = mid +1 ;
+            }
+            if(target == arr[mid]){
+                return mid;
+            }
+        }
+        return start;
+    }
+}
