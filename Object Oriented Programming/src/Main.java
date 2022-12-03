910b9c3c6a4f14801c3b68428a52e6a4689581fb
SaurabPoudel <poudelsaurab20@gmail.com>
2022-12-03 20:00:39 +0545

    Linked list and Object Oriented programming

---
+++ Object Oriented Programming/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // stores five roll numbers
+        int[] numbers = new int[5];
+        // stores five names
+        String[] names = new String[5];
+
+    }
+}
